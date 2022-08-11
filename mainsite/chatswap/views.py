543a141113583853f79cdd6f8bb388cb2346514e
TomasAgustin95 <jcarlosblanco4@gmail.com>
2022-08-11 01:10:01 -0400

    Implemented token list in token select modal sourced from coingecko.

--- mainsite/chatswap/views.py
+++ mainsite/chatswap/views.py
@@ -1,5 +1,11 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
+import chatswap.swapscripts.tokenlist as tokenlist
 
 def index(request):
-    return render(request, "chatswap/index.html")
+    tokens = tokenlist.getTokenData("https://tokens.coingecko.com/uniswap/all.json")
+    tokenTickers = []
+    for i in tokens:
+        tokenTickers.append(i["ticker"])
+    context = {"tokenNames": tokenTickers}
+    return render(request, "chatswap/index.html", context)
